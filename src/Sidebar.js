c14d3875c2a6dcf42f7d083e3566ca19842f28b8
Salman <salman@gmail.com>
2020-12-13 02:03:05 +0500

    --- Auto-Git Commit ---

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,29 @@
+import { EmojiFlags, Storefront, VideoLibrary } from '@material-ui/icons';
 import React from 'react'
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+
+
+
+
+
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow/>
-            <SidebarRow/>
-            <SidebarRow/>
-            <SidebarRow/>
-            <SidebarRow/>
+            <SidebarRow src={user.photoURL} title={user.displayname}/>
+            <SidebarRow Icon={EmojiFlagIcon} title="Pages"/>
+            <SidebarRow Icon={PeopleIcon} title="Friends"/>
+            <SidebarRow Icon={ChatroomIcon} title="Messanger"/>
+            <SidebarRow Icon={StorefrontIcon} title="MarketPlace"/>
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos"/>
+            <SidebarRow Icon={ExpendMoreOutlined} title="MarketPlace"/>
+            
         </div>
     )
 }
