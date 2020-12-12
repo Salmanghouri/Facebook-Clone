f2c4c23a8ab331f0b30c0809cc885661a8d97587
Salman <salman@gmail.com>
2020-12-13 02:33:12 +0500

    --- Auto-Git Commit ---

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,12 @@ import React from 'react'
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
 import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import EmojiFlagIcon from "@material-ui/icons/EmojiFlags"
+import PeopleIcon from "@material-ui/icons/People"
+import ChatroomIcon from "@material-ui/icons/Chat"
+import StorefrontIcon from "@material-ui/icons/Storefront"
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary"
+import ExpendMoreOutlined from "@material-ui/icons/ExpandMore"
 
 
 
@@ -16,7 +19,8 @@ import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow src={user.photoURL} title={user.displayname}/>
+            <SidebarRow src="https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/105004551_881222059049481_367761390988446037_n.jpg?_nc_cat=104&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeEDvPT_r92vjlvv_MP3YhG7sImn1S6Sq5ywiafVLpKrnLPeT2ZXXusbTmfs91eUXNkDHwqB2vKREBel5PfOOPW-&_nc_ohc=nGO7Bg8p150AX99IyhB&_nc_ht=scontent.fkhi6-1.fna&oh=a02db3940409732c49cb91a52ba350da&oe=5FF9A2DF" 
+            title="Salman Ghouri"/>
             <SidebarRow Icon={EmojiFlagIcon} title="Pages"/>
             <SidebarRow Icon={PeopleIcon} title="Friends"/>
             <SidebarRow Icon={ChatroomIcon} title="Messanger"/>
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -2,12 +2,12 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 import "./SidebarRow.css";
 
-
 function SidebarRow({src, Icon, title}) {
     return (
         <div className="sidebarRow">
             {src && <Avatar src={src}/>}
             {Icon && <Icon/>}
+            
             <p>{title}</p>
         </div>
     )
