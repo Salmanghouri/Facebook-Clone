dd7a412e0127cf79251effc4bea3b5d49d2ed938
Salman <salman@gmail.com>
2020-12-12 23:17:49 +0500

    --- Auto-Git Commit ---

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import "./Header.css";
 import Searchicon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscribtionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlined from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+
+
 
 function Header() {
     return <div className="header">
@@ -14,11 +27,48 @@ function Header() {
 
         </div>
     </div>
+    
 
-    <div className="header__middle"></div> 
+    <div className="header__middle">
+    <div className="header__option">
+        <HomeIcon fontSize="Large"/>
+        </div> 
+        <div className="header__option">
+        <FlagIcon fontSize="Large"/>
+        </div>   
+        <div className="header__option">
+        <SubscribtionsOutlinedIcon fontSize="Large"/>
+        </div>
+        <div className="header__option">
+        <StorefrontOutlined fontSize="Large"/>
+        </div>
+        <div className="header__option">
+        <SupervisedUserCircleIcon fontSize="Large"/>
+        </div> 
+        
+    </div> 
 
-    <div className="header__right"></div>    
-    </div>;
+    <div className="header__right">
+        <div className="header__info">
+            <Avatar />
+            <h4>Salman Ghouri</h4>
+        </div>
+        <IconButton>
+            <AddIcon />
+        </IconButton>
+        <IconButton>
+            <ForumIcon />
+        </IconButton>
+        <IconButton>
+            <AddIcon />
+        </IconButton>
+        <IconButton>
+            <AddIcon />
+        </IconButton>
+
+
+        </div>    
+    </div>
 }
 
 export default Header
