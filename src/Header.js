6acb0c8cd36b02c288645e4009874505188d43f2
Salman <salman@gmail.com>
2020-12-12 18:08:31 +0500

    facebookclone1212

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css";
+
+function Header() {
+    return <div className="header">
+    <div className="header__left">
+        <img src="https://www.flaticon.com/svg/static/icons/svg/124/124010.svg" 
+        alt=""
+        />
+    </div>
+
+    <div className="header__middle"></div> 
+
+    <div className="header__right"></div>    
+    </div>;
+}
+
+export default Header
+
+
