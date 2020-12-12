982d65f5f762b31ea5b856f8d9db3523bb368835
Salman <salman@gmail.com>
2020-12-13 01:32:57 +0500

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import react from 'react'
 import './App.css';
 import Header from "./Header";
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-     <Header />
-      {/* App body */}
-       {/* Slider */}
+     <Header /> 
+     <div className="app__body">
+       <Sidebar/>
+
+ 
         {/* feed*/}
         {/* Widges*/}
 
-
+     </div>
+    
     </div>
   );
 }
