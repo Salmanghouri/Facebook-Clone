ce221e567b04f10c534af70d5609b7cbb4eceab5
Salman <salman@gmail.com>
2020-12-14 18:28:24 +0500

    --- Auto-Git Commit ---

--- src/Story.js
+++ src/Story.js
@@ -1,16 +1,16 @@
-import { Avatar } from '@material-ui/core';
-import React from "react";
+import {Avatar} from '@material-ui/core';
+import React from 'react'
 import "./Story.css";
 
-function Story({ image, profileSrc, title }) {
+function Story({image, profileSrc, title}) {
     return (
-        <div style={{ backgroundImage: "url(${image})" }}
-         className="Story">
-            <Avatar className="story__avatar" src={profileSrc}/>
-            <h4>{title}</h4>
-
-        </div>
+        <div style={{ backgroundImage: 'url(${image})' }}
+        className="Story">
+           <Avatar className="story__avatar" src={profileSrc}/>
+           <h4>{title}</h4>
+           </div>
     );
+    
 }
 
 export default Story;
