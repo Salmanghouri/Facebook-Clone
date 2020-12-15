a9ff39c2566a34457654d39853c90e8cfa7f7892
Salman <salman@gmail.com>
2020-12-15 15:29:18 +0500

    --- Auto-Git Commit ---

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,10 +1,31 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import "./MessageSender.css";
 
 function messageSender() {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
     return (
-        <div messageSender="messangeSender">
-            <div className>
+        <div className="messangeSender">
+            <div className="messageSender_top">
+
+
+            </div>
+            <div className="messageSender_bottom">
+                <Avatar />
+                <form>
+                    <input className="MessageSender__input"
+                    placeholder={'What is on your mind'}/>
+                    
+                    
+                    <input placeholder="image URL (Optional)"/>
+                    <button onClick={handleSubmit} type="Submit" >
+                        Hidden button
+                    </button>
+                </form>
+
 
             </div>
             
