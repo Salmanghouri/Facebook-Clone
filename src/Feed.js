796cde635944e5ad0a0f5e2f995c207cda5a291a
Salman <salman@gmail.com>
2020-12-17 17:21:15 +0500

    --- Auto-Git Commit ---

--- src/Feed.js
+++ src/Feed.js
@@ -9,6 +9,7 @@ function Feed() {
         <div className="feed">
             <StoryReel/>
             <MessageSender/>
+            
               {/*Messange Sender */}
 
         </div>
--- src/Story.js
+++ src/Story.js
@@ -4,7 +4,7 @@ import "./Story.css";
 
 function Story({ image, profileSrc, title }) {
     return (
-        <div style={{ backgroundImage: 'url(${image})' }}
+        <div 
          className="Story">
             <Avatar className="story__avatar" src={profileSrc}/>
             <h4>{title}</h4>
